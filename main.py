5065734fe7d788bd25f6a2a6ef2230c1557b0825
Dan Jenkins <djenkinsjg0617@gmail.com>
2023-09-26 21:05:40 -0500

    Working

--- main.py
+++ main.py
@@ -1,94 +1,9 @@
-from typing import Union, Annotated
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-
-import schema
-from database import SessionLocal, engine
-import models
-
-#models.Base.metadata.create_all(bind=engine)
-
-# def get_database_session():
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
-
-from fastapi.middleware.cors import CORSMiddleware
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:80",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-# @app.post("/items")
-# async def create_item(item: Item):
-#     item_dict = item.dict()
-#     if item.tax:
-#         price_with_tax = item.price + item.tax
-#         item_dict.update({"price_with_tax": price_with_tax})
-#     return item_dict
-
-
-
-@app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
-
-
-@app.post("/login/")
-def read_credentials(username: str, password: str):
-    return {"Status": "Logged in"}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+async def root():
+    return {"message": "Hello World"}
