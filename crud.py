4865e35fbf0437130710e23cb7691710c42d4c3e
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-04 21:46:34 -0500

    Now storing profile picture link

--- crud.py
+++ crud.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import Session
 
-#from . import models, schemas
-#import models, schemas
+# from . import models, schemas
+# import models, schemas
 import models
 
 import schemas
 
 import middleware
+
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -24,17 +25,18 @@ def create_user(db: Session, user: schemas.CreateUser):
 
     print(user)
     # Set up variables to pass to the model
-    #fake_hashed_password = user.password + "notreallyhashed"
+    # fake_hashed_password = user.password + "notreallyhashed"
     hashed_password = middleware.get_password_hash(user.password)
 
-    #create user model
-    #db_user = models.User(email=user.email, hashed_password=fake_hashed_password, picture_path = user.picture_path)
+    # create user model
+    # db_user = models.User(email=user.email, hashed_password=fake_hashed_password, picture_path = user.picture_path)
     db_user = models.User(
         first_name=user.first_name,
         last_name=user.last_name,
         phone_number=user.phone_number,
         email=user.email,
-        hashed_password = hashed_password
+        profile_picture_link=user.profile_picture_id,
+        hashed_password=hashed_password
     )
     # add to DB
     db.add(db_user)
--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 
 import database
 
+
 class User(database.Base):
     __tablename__ = "users"
 
@@ -12,8 +13,6 @@ class User(database.Base):
     last_name = Column(String(255))
     phone_number = Column(String(255))
     email = Column(String(255), unique=True, index=True)
+    profile_picture_link = Column(String(255), unique=True)
     hashed_password = Column(String(255))
-    #picture_path = Column(String(255))
-
-
-
+    # picture_path = Column(String(255))
