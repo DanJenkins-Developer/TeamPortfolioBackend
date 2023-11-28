c13c8dff7d44dbf0e719a5fc520a29efd31bd445
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-28 17:54:24 -0600

    startup script

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-# Add the Deadsnakes PPA
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-
-
-# Install a specific Python version, e.g., Python 3.9
-apt install python3.9
-
-# Upgrade pip for the installed Python version
-python3.9 -m pip install --upgrade pip
-
-# Install requirements using the newly installed Python version
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+pip install --upgrade pip
+pip install -r requirements.txt
