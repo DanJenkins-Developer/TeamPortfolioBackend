239db1c9d3d3f2acfac03e96a14c0a322a4797ba
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-28 12:41:06 -0600

    basic stripe process-payment

--- main.py
+++ main.py
@@ -137,6 +137,32 @@ async def read_users_me(current_user: models.User = Depends(authorize)):
     return current_user
 
 
+@app.post("/process-payment")
+async def process_payment(amount: int, currency: str, token: str):
+    try:
+        # Create a charge using the Stripe API
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency=currency,
+            # Stripe token obtained from the client-side (e.g., Stripe.js)
+            source=token,
+            description="Payment for FastAPI Store",  # Add a description for the payment
+        )
+
+        # You can handle the charge object as per your requirements
+        # For example, log the payment or perform other actions
+
+        # Return a success response
+        return {"status": "success", "charge_id": charge.id}
+
+    except stripe.error.CardError as e:
+        # Handle specific Stripe errors
+        return {"status": "error", "message": str(e)}
+    except stripe.error.StripeError as e:
+        # Handle generic Stripe errors
+        return {"status": "error", "message": "Something went wrong. Please try again later."}
+
+
 # @app.get("/users/items")
 # async def read_own_items(current_user: schemas.User = Depends(middleware.get_current_active_user)):
 #     return [{"item_id": 1, "owner":current_user}]
